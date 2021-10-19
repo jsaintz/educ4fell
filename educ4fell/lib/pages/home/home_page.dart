264de7b8e16b033d1655d27912c4351a06094bda
jonatas.santos <jonatas.santos@teladoc.com>
2021-10-19 00:27:44 -0300

    include drawer

--- educ4fell/lib/pages/home/home_page.dart
+++ educ4fell/lib/pages/home/home_page.dart
@@ -10,6 +10,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("Minha conta"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("Professor"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.paste),
+              title: const Text("Relatórios"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: const Text("Alunos"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: const Text("Ajuda"),
+              onTap: () {
+                Navigator.pop(context);
+                //Navegar para outra página
+              },
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: const Text('Bem vindo'),
+      ),
+    );
   }
 }
